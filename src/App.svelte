d2dd007a2489e2846a76a35332d3f5cf523aa264
Sa7cez <sa7cez@gmail.com>
2024-01-17 04:55:08 +0500

    changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
   import axios from 'axios'
   
   let server = 'https://api-polygon.reservoir.tools'
   let addresses : string = '0x7DBB717cEa1D178e455E650c824b76817cD2041B\n0xA77851ea917A923eD495bD704707c5E274161dE0\n0x9a0BC9B906c4F74C290e3F5Ba4F4254fAe1A7E76'
   let collection = '0x89acaf092909d5e20a18fd6b4a866e3674495b9e'
 
-  let checked : string[] = []
+  let checked = ''
+  let empty = ''
 
-  const sleep = async (time) => new Promise((resolve) => setTimeout(() => resolve(true), time))
+  const sleep = async (time: number) => new Promise((resolve) => setTimeout(() => resolve(true), time))
 
   const check = async () => {
-    const getUserTokens = async (address: Hex) =>
+    checked = ''
+    empty = ''
+    const getUserTokens = async (address: string) : Promise<any> =>
       axios.get(`${server}/users/${address}/tokens/v9?collection=${collection}`)
         .then(({ data }: any) => {
           if (data.tokens.length > 0) {
-            checked += `${address} ${data.tokens.map((i: any) => `${i.token.tokenId} ${i.token.name}`)}\n`
+            checked += `${address}: ${data.tokens.map((i: any) => `${i.token.tokenId} ${i.token.name}`)}\n`
           } else {
+            empty += `${address}\n`
             console.log(address, `doesn't have NFT`)
             addresses = forCheck.join('\n')
           }
@@ -33,7 +34,7 @@
     console.log(forCheck)
     while (forCheck.length > 0) {
       const address = forCheck.pop()
-      const tokens = await getUserTokens(address)
+      await getUserTokens(address as string)
     }
   }
   
@@ -42,12 +43,12 @@
 
 <main>
   <div>
-    <label>Server:</label>
+    <label><a href="https://docs.reservoir.tools/reference/supported-chains" target="_blank">Server:</a></label>
     <input bind:value={server} placeholder="Reservoir server"/>
   </div>
 
   <div>
-    <label>Collection contract/name:</label>
+    <label>Collection address/name:</label>
     <input bind:value={collection} placeholder="Collection address"/>
   </div>
   
@@ -60,9 +61,19 @@
     <button on:click={check}>NFT Token & Name Check!</button>
   </div>
 
+  {#if checked.length > 0}
   <div>
+    <label>With NFT:</label>
     <textarea placeholder="Results here" rows="10">{checked}</textarea>
   </div>
+  {/if}
+
+  {#if empty.length > 0}
+  <div>
+    <label>Without NFT:</label>
+    <textarea placeholder="Results here" rows="3">{empty}</textarea>
+  </div>
+  {/if}
 
 </main>
 
@@ -76,6 +87,10 @@
     margin: 0.5rem;
   }
   button {
+    width: 100%;
     margin: 0;
+  }
+  label {
+    display: block;
   }
 </style>
